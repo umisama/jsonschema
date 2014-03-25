d1f9dd3955c5949782f90a1e6d07d891b1b5ef3e
umisama <umisama@fe2o3.jp>
2014-03-25 19:01:36 +0900

    impl for passing #1

--- jsonschema.go
+++ jsonschema.go
@@ -1,30 +1,137 @@
 package json
 
-type JsonType int64
-const (
-	JsonType_Bool = JsonType(iota)
-	JsonType_Number
-	JsonType_String
-	JsonType_Array
-	JsonType_Object
-	JsonType_Nil
+import (
+	"encoding/json"
+	"errors"
 )
 
-func (j JsonType)String() string {
-	switch j {
-	case JsonType_Bool:
-		return "booleans"
-	case JsonType_Number:
-		return "numbers"
-	case JsonType_String:
-		return "strings"
-	case JsonType_Array:
-		return "arrays"
-	case JsonType_Object:
-		return "objects"
-	case JsonType_Nil:
-		return "null"
-	default:
-		return "unknown"
+// JsonType reprecents json schema's primitive types.
+// defined at
+type JsonType string
+
+const (
+	JsonType_Bool    = JsonType("boolean")
+	JsonType_Number  = JsonType("number")
+	JsonType_Integer = JsonType("integer")
+	JsonType_String  = JsonType("string")
+	JsonType_Array   = JsonType("array")
+	JsonType_Object  = JsonType("object")
+	JsonType_Null    = JsonType("null")
+	JsonType_INVALID = JsonType("system-Invalid")
+)
+
+var (
+	ErrInvalidTypeName = errors.New("jsonschema: invalid type name")
+)
+
+func GetJsonType(typestr string) (t JsonType, err error) {
+	types := []JsonType{JsonType_Bool, JsonType_Number, JsonType_Integer, JsonType_String, JsonType_Array, JsonType_Object, JsonType_Null}
+	for _, v := range types {
+		if v.String() == typestr {
+			return v, nil
+		}
 	}
+
+	return JsonType_INVALID, ErrInvalidTypeName
+}
+
+func (j JsonType) String() string {
+	return string(j)
+}
+
+func (j JsonType) IsValidType(v interface{}) (ret bool) {
+	switch j {
+	case JsonType_Array:
+		_, ret = v.([]interface{})
+	case JsonType_Bool:
+		_, ret = v.(bool)
+	case JsonType_Integer:
+		_, ret = v.(int)
+	case JsonType_Null:
+		// TODO
+		ret = false
+	case JsonType_Number:
+		_, ret = v.(float64)
+	case JsonType_Object:
+		_, ret = v.(map[string]interface{})
+	case JsonType_String:
+		_, ret = v.(string)
+	default:
+		ret = false
+	}
+
+	return
+}
+
+type schemaRoot struct {
+	schema, title, ref, description string
+	jsontype                        JsonType
+	required, isarray               bool
+	child                           map[string]schemaProperties
+}
+
+type schemaProperties struct {
+	val map[string]schemaProperties
+}
+
+type Validator struct {
+	schema *schemaRoot
+}
+
+func NewValidator(schema []byte) (v *Validator, err error) {
+	v = new(Validator)
+
+	schema_raw := make(map[string]interface{})
+	err = json.Unmarshal(schema, &schema_raw)
+	if err != nil {
+		return
+	}
+
+	v.schema, err = v.parseSchema(schema_raw)
+
+	return
+}
+
+func (v *Validator) parseSchema(json map[string]interface{}) (schema *schemaRoot, err error) {
+	schema = &schemaRoot{}
+
+	schema.title, _ = json["title"].(string)
+	schema.schema, _ = json["$schema"].(string)
+	schema.description, _ = json["description"].(string)
+
+	jsontype, _ := json["type"].(string)
+	schema.jsontype, err = GetJsonType(jsontype)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func (v *Validator) IsValid(jsonstr []byte) bool {
+	var jsonobj interface{}
+	err := json.Unmarshal(jsonstr, &jsonobj)
+	if err != nil {
+		println(err.Error())
+		return false
+	}
+
+	result := true
+	check := func(v *schemaRoot, obj interface{}) {
+		if !v.jsontype.IsValidType(obj) {
+			result = false
+			return
+		}
+
+		// go next if array or object
+		switch v.jsontype {
+		case JsonType_Array:
+			//TODO
+		case JsonType_Object:
+			//TODO
+		}
+	}
+	check(v.schema, jsonobj)
+
+	return result
 }
--- jsonschema_test.go
+++ jsonschema_test.go
@@ -10,19 +10,66 @@ type TestCaseJsonType struct {
 }
 
 var TestCasesJsonType_String = []TestCaseJsonType{
-	TestCaseJsonType{JsonType_Bool, "booleans"},
-	TestCaseJsonType{JsonType_Nil, "null"},
-	TestCaseJsonType{JsonType_Number, "numbers"},
-	TestCaseJsonType{JsonType_Array, "arrays"},
-	TestCaseJsonType{JsonType_Object, "objects"},
-	TestCaseJsonType{JsonType_String, "strings"},
+	TestCaseJsonType{JsonType_Bool, "boolean"},
+	TestCaseJsonType{JsonType_Null, "null"},
+	TestCaseJsonType{JsonType_Number, "number"},
+	TestCaseJsonType{JsonType_Integer, "integer"},
+	TestCaseJsonType{JsonType_Array, "array"},
+	TestCaseJsonType{JsonType_Object, "object"},
+	TestCaseJsonType{JsonType_String, "string"},
 }
 
-func Test_Test(t *testing.T) {
+type TestCaseExampleData struct {
+	input, schema string
+	isvalid       bool
+}
+
+// testdatas from examples on http://json-schema.org/example1.html.
+// ref) http://json-schema.org/example1.html
+var TestCasesExampleData = []TestCaseExampleData{
+	TestCaseExampleData{
+		`{
+			"id": 1,
+			"name": "A green door",
+			"price": 12.50,
+			"tags": ["home", "green"]
+		}`,
+		`{
+			"$schema": "http://json-schema.org/draft-04/schema#",
+			"title": "Product",
+			"description": "A product from Acme's catalog",
+			"type": "object"
+		}`,
+		true,
+	},
+}
+
+func Test_JsonType_String(t *testing.T) {
 	for k, v := range TestCasesJsonType_String {
 		if v.jsontype.String() != v.ret {
 			t.Error("fail on", k)
 		}
 	}
 	return
+}
+
+func Test_NewValidator(t *testing.T) {
+	t.Skip()
+	schema := []byte(`{}`)
+	val, err := NewValidator(schema)
+	println(val, err)
+	return
+}
+
+func Test_ExampleDatas(t *testing.T) {
+	for k, v := range TestCasesExampleData {
+		validator, err := NewValidator([]byte(v.schema))
+		if err != nil {
+			t.Error("fail on", k, "with", err)
+		}
+
+		if validator.IsValid([]byte(v.input)) != v.isvalid {
+			t.Error("fail on", k)
+		}
+	}
 }
